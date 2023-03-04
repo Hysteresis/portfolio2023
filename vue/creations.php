91cbcf63f907eec3fc87509ff89d3cc4b125d39d
Legenda\Pierrot <piballz@gmail.com>
2023-03-04 14:43:47 +0100

    MAJ

--- vue/creations.php
+++ vue/creations.php
@@ -58,11 +58,21 @@
         <div class="row  g-4  gy-md-5 gy-lg-4 ">
             <div class="col-12 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center align-items-center">
                 <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="./img/apiPlanetHome.png" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title">API Planète</h5>
+                        <p class="card-text">Manipulation de fichiers json et du Modèle MVC : <a href="api.le-systeme-solaire.net">api.le-systeme-solaire.net/</a></p>
+                        <a href="./systeme_solaire/index.php" target="_blank" class="btn btn-primary">Go Système solaire</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center align-items-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="./img/lolHome.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Dragon rift</h5>
+                        <p class="card-text">Manipulation des APIs de Riot. Système de recherche par Invocateur</p>
+                        <a href="#" class="btn btn-primary">Go Dragon rift</a>
                     </div>
                 </div>
             </div>
@@ -76,66 +86,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center align-items-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center align-items-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center align-items-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center align-items-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center align-items-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center align-items-center">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
+
 
         </div>
     </div>
